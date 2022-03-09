85ac07f148c339b1315f27ad042f895d0ccde747
Kristian Santos <kristianmoacir@hotmail.com>
2022-03-09 17:55:27 -0300

    Add source code

---
+++ rails/app/product_manager/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProductManager::Application.config.secret_key_base = '10e243539e43557cfa9252edfa899d9bada352a9e4e7d8f03bf36610d30cfcfdffff82995f21b644cd6a0fb46d058ac9c159a70047df57d2c69d07af42195073'
---
+++ rails/app/product_manager/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ProductManager::Application.config.session_store :cookie_store, key: '_product_manager_session'

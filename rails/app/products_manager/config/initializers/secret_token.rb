81edd49aa16fbf603a56e8bd877cbd6cf6a3b6ba
Kristian Santos <kristianmoacir@hotmail.com>
2022-03-10 16:19:45 -0300

    Add core project

---
+++ rails/app/products_manager/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProductsManager::Application.config.secret_key_base = 'd64291bf94a79df1580117bf00b6919d163ae77839646bb701c8610410903e0c3a4fc366368c02d62b7c3dc7e9a64ab0ab95fe59ba615d6cddd2eea8b6c9e7a4'

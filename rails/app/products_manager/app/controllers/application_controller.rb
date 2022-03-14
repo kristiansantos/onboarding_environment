8f7cf79c64a9ef32f255e19d2068b93ac49f794b
Kristian Santos <kristianmoacir@hotmail.com>
2022-03-14 15:14:59 -0300

    feat: add error handler in api and products controller

--- rails/app/products_manager/app/controllers/application_controller.rb
+++ rails/app/products_manager/app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+  rescue_from StandardError, with: :render_internal_server_error
+
+  def render_internal_server_error(exception)
+    Rails.logger.debug exception
+    render json: { error: 'Something went wrong, please try again or contact our support.' },
+           status: :internal_server_error
+  end
 end
--- rails/app/products_manager/app/controllers/products_controller.rb
+++ rails/app/products_manager/app/controllers/products_controller.rb
@@ -41,13 +41,15 @@ class ProductsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_product
-      @product = Product.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def product_params
-      params.require(:product).permit(:sku, :name, :description, :amount, :price)
-    end
+  def set_product
+    @product = Product.find(params[:id])
+  rescue Mongoid::Errors::DocumentNotFound
+    render json: { error: 'Not Found' }, status: :not_found
+  end
+
+
+  def product_params
+    params.require(:product).permit(:sku, :name, :description, :amount, :price)
+  end
 end

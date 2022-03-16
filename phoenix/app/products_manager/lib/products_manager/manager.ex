57d4c58cd462e0cbe81d9ab04613d723a5d3fd64
Kristian Santos <kristianmoacir@hotmail.com>
2022-03-16 16:47:50 -0300

    feat(second_challenge): add product service

---
+++ phoenix/app/products_manager/lib/products_manager/manager.ex
@@ -0,0 +1,34 @@
+defmodule ProductsManager.Manager do
+
+  import Ecto.Query, warn: false
+  alias ProductsManager.Repo
+
+  alias ProductsManager.Manager.Product
+
+
+  def list_products do
+    Repo.all(Product)
+  end
+
+  def get_product!(id), do: Repo.get!(Product, id)
+
+  def create_product(attrs \\ %{}) do
+    %Product{}
+    |> Product.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def update_product(%Product{} = product, attrs) do
+    product
+    |> Product.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete_product(%Product{} = product) do
+    Repo.delete(product)
+  end
+
+  def change_product(%Product{} = product, attrs \\ %{}) do
+    Product.changeset(product, attrs)
+  end
+end
---
+++ phoenix/app/products_manager/lib/products_manager_web/views/product_view.ex
@@ -0,0 +1,21 @@
+defmodule ProductsManagerWeb.ProductView do
+  use ProductsManagerWeb, :view
+  alias ProductsManagerWeb.ProductView
+
+  def render("index.json", %{products: products}) do
+    %{data: render_many(products, ProductView, "product.json")}
+  end
+
+  def render("show.json", %{product: product}) do
+    %{data: render_one(product, ProductView, "product.json")}
+  end
+
+  def render("product.json", %{product: product}) do
+    %{id: product.id,
+      sku: product.sku,
+      name: product.name,
+      description: product.description,
+      amount: product.amount,
+      price: product.price}
+  end
+end

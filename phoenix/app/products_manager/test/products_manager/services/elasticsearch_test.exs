0f64aad7aa9e26164ccd5d6b25de8f986eceb615
Kristian Santos <kristianmoacir@hotmail.com>
2022-05-04 10:30:58 -0300

    test: add tirexs uri in test

--- phoenix/app/products_manager/test/products_manager/services/elasticsearch_test.exs
+++ phoenix/app/products_manager/test/products_manager/services/elasticsearch_test.exs
@@ -39,21 +39,22 @@ defmodule ProductsManager.ElasticSearchTest do
 
   describe "create_or_update/2" do
     test "With sucess to check url" do
-      assert "/products_manager_test/data_source/#{@valid_attrs.id}" ==
-               "#{Application.get_env(:elasticsearch, :index)}/#{@source}/#{@valid_attrs.id}"
+      assert "http://127.0.0.1:9200/products_manager_test/data_source/#{@valid_attrs.id}" ==
+               "#{Application.get_env(:tirexs, :uri)}#{Application.get_env(:elasticsearch, :index)}/#{@source}/#{@valid_attrs.id}"
     end
   end
 
   describe "delete/2" do
     test "With sucess to check url" do
-      assert "/products_manager_test/data_source/#{@valid_attrs.id}" ==
-               "#{Application.get_env(:elasticsearch, :index)}/#{@source}/#{@valid_attrs.id}"
+      assert "http://127.0.0.1:9200/products_manager_test/data_source/#{@valid_attrs.id}" ==
+               "#{Application.get_env(:tirexs, :uri)}#{Application.get_env(:elasticsearch, :index)}/#{@source}/#{@valid_attrs.id}"
     end
   end
 
   describe "delete_all/0" do
     test "With sucess to check url" do
-      assert "/products_manager_test" == "#{Application.get_env(:elasticsearch, :index)}"
+      assert "http://127.0.0.1:9200/products_manager_test" ==
+               "#{Application.get_env(:tirexs, :uri)}#{Application.get_env(:elasticsearch, :index)}"
     end
   end
 end

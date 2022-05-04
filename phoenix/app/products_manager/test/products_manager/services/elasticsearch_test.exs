c9e0aecdd5ef411702df6514b839e0f1ff9f3ce9
Kristian Santos <kristianmoacir@hotmail.com>
2022-05-04 10:25:28 -0300

    test: add new tests in elasticsearch service

--- phoenix/app/products_manager/test/products_manager/services/elasticsearch_test.exs
+++ phoenix/app/products_manager/test/products_manager/services/elasticsearch_test.exs
@@ -16,22 +16,44 @@ defmodule ProductsManager.ElasticSearchTest do
     updated_at: DateTime.to_iso8601(DateTime.utc_now())
   }
 
-  @source "product"
+  @source "data_source"
 
   setup :verify_on_exit!
 
-  describe "get_all" do
-    test "get_all/1 only source" do
+  describe "get_all/1" do
+    test "With sucess" do
       tirexs_mock_get(:ok, @valid_attrs)
 
       assert {:ok, [@valid_attrs]} == Elasticsearch.get_all(@source)
     end
 
-    test "get_all/2 filter and source" do
+  end
+
+  describe "get_all/2" do
+
+    test "With sucess and apply filters" do
       tirexs_mock_get(:ok, @valid_attrs)
 
       assert {:ok, [@valid_attrs]} ==
                Elasticsearch.get_all(@source, name: "name_test", barcode: "UP77BR56")
+    end
+  end
+
+  describe "create_or_update/2" do
+    test "With sucess to check url" do
+      assert "/products_manager_test/data_source/#{@valid_attrs.id}" == "#{Application.get_env(:elasticsearch, :index)}/#{@source}/#{@valid_attrs.id}"
+    end
+  end
+
+  describe "delete/2" do
+    test "With sucess to check url" do
+      assert "/products_manager_test/data_source/#{@valid_attrs.id}" == "#{Application.get_env(:elasticsearch, :index)}/#{@source}/#{@valid_attrs.id}"
+    end
+  end
+
+  describe "delete_all/0" do
+    test "With sucess to check url" do
+      assert "/products_manager_test" == "#{Application.get_env(:elasticsearch, :index)}"
     end
   end
 
